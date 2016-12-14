da0b040f4694c60ca1e40f7709ad2ce01defa0cd
Allan Roberto <allan.rsti@gmail.com>
2016-12-14 14:34:16 -0300

    Seguradoras

--- wordpress/wp-content/themes/Divi-child/functions.php
+++ wordpress/wp-content/themes/Divi-child/functions.php
@@ -9,3 +9,9 @@
             wp_enqueue_script( 'icheck-jquery',get_template_directory_uri(). '-child/js/icheck.min.js', array( 'jquery', 'divi-script' ), '0.1.2', true );
 
 	}
+
+	if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page();
+	
+}
