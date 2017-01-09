2ad80f242bca2ce4102f9233f759ce55a17e363d
Allan Roberto <allan.rsti@gmail.com>
2017-01-09 10:29:32 -0300

    Menu

--- wordpress/wp-content/themes/Divi-child/functions.php
+++ wordpress/wp-content/themes/Divi-child/functions.php
@@ -2,13 +2,13 @@
 	add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 	function theme_enqueue_styles() {
 	    wp_enqueue_style( 'divi', get_template_directory_uri() . '/style.css' );
-      wp_enqueue_script( 'divi-script',get_template_directory_uri(). '-child/js/scripts.js', array( 'jquery' ), '0.1.2', true );
+      wp_enqueue_script( 'divi-script',get_template_directory_uri(). '-child/js/scripts.js', array( 'jquery' ), '0.1.3', true );
 
-      wp_enqueue_script( 'jquery-ui',get_template_directory_uri(). '-child/js/jquery-autocomplete/jquery-ui.js', array( 'jquery' ), '0.1.2', true );
+      wp_enqueue_script( 'jquery-ui',get_template_directory_uri(). '-child/js/jquery-autocomplete/jquery-ui.js', array( 'jquery' ), '0.1.3', true );
 
-            wp_enqueue_script( 'maskinput-jquery',get_template_directory_uri(). '-child/js/maskinput-jquery.js', array( 'jquery', 'divi-script' ), '0.1.2', true );
+            wp_enqueue_script( 'maskinput-jquery',get_template_directory_uri(). '-child/js/maskinput-jquery.js', array( 'jquery', 'divi-script' ), '0.1.3', true );
 
-            wp_enqueue_script( 'icheck-jquery',get_template_directory_uri(). '-child/js/icheck.min.js', array( 'jquery', 'divi-script' ), '0.1.2', true );
+            wp_enqueue_script( 'icheck-jquery',get_template_directory_uri(). '-child/js/icheck.min.js', array( 'jquery', 'divi-script' ), '0.1.3', true );
 
 	}
 
