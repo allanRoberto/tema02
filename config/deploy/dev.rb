d1346411c837036ea7335084b52581ce0ce1fa07
Allan Roberto <allan.rsti@gmail.com>
2017-05-11 08:20:29 -0400

    Final tema 02.1

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -2,14 +2,14 @@
 set :deploy_to, "/home/solucoesparacorretores/sites/#{application}-#{stage}"
 
 # Use the correct branch on github. Uncomment this if you have set up seperate branches for each staging area
-set :branch, "dev" 
+set :branch, "prod" 
 
 # The hostnames to deploy to.
-role :web, "ftp.solucoesparacorretores.com.br"
+role :web, "ftp.web2003.uni5.net"
 
 # Specify one of the web servers to use for database backups or updates.
 # This server should also be running Wordpress.
-role :db, "ftp.solucoesparacorretores.com.br", :primary => true 
+role :db, "ftp.web2003.uni5.net", :primary => true 
 
 # The path to wp-cli
 set :wp, "wp" #(Should be the full path to the wp command on your server)
